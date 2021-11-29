45f64330cf6675a5716c33c6bbd0e9106e610616
Lukasz Grabski <lukasz.grabski@capgemini.com>
2021-11-29 11:16:23 +0100

    Final Release

---
+++ CastDice/Dice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastDice
+{
+    class Dice
+    {
+        public int Cast { get; set; }
+        public string Name { get; set; } 
+
+        private const int castMin = 1;
+        private const int castMax = 7; //max nie należy do zbioru
+
+        private readonly Random _random = new Random();
+
+        public Dice(string nameInput, int valueInput)
+        {
+            Cast = valueInput;
+            Name = nameInput;
+        }
+
+        public int RandomNumber(int min, int max)
+        {
+            return _random.Next(min, max);
+        }
+
+        public void CastDice()
+        {
+            int _randomValue = RandomNumber(castMin, castMax);
+            this.Cast = _randomValue;
+        }
+
+        public void PrintDiceCast()
+        {
+            Console.WriteLine($"Wartość {this.Name} wynosi {this.Cast}");
+        }
+    }
+}
--- CastDice/Program.cs
+++ CastDice/Program.cs
@@ -4,9 +4,33 @@ namespace CastDice
 {
     class Program
     {
+        private string firstDiceName = "Kość nr 1";
+        private string secondDiceName = "Kość nr 2";
+
+        private static int diceInitialValue = 1;
+        private static int amountOfCasts = 0;
+
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program DiceCasting = new Program();
+
+            Dice FirstDice = new Dice(DiceCasting.firstDiceName,diceInitialValue);
+            Dice SecondDice = new Dice(DiceCasting.secondDiceName, diceInitialValue);
+
+            do 
+            {
+                FirstDice.CastDice();
+                SecondDice.CastDice();
+
+                FirstDice.PrintDiceCast();
+                SecondDice.PrintDiceCast();
+
+                amountOfCasts++;
+
+            } while (FirstDice.Cast != 6 || SecondDice.Cast != 6);
+
+            Console.WriteLine($"Wylosowano 2 razy 6. Ilość rzutów kością: {amountOfCasts}");
         }
     }
 }
