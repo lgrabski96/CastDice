9cb351b4694fdeea490a8b9b59adad32367af79f
Lukasz Grabski <lukasz.grabski@capgemini.com>
2021-11-29 10:07:46 +0100

    Add project files.

---
+++ CastDice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CastDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
